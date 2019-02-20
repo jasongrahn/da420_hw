fb6c8d478588753fc085b5157c9df3db099a71e6
Jason Grahn <jasongrahn@gmail.com>
2019-02-20 02:47:57 +0000

    i dont know.

--- project07/api_token_gather.R
+++ project07/api_token_gather.R
@@ -1,4 +1,11 @@
+## ---- apikey ----
 api_key <- getPass::getPass(msg = "Gimme that API key")
+
+## ---- api_secret ----
 api_secret <- getPass::getPass(msg = "Gimme that API Secret")
+
+## ---- token ----
 token <- getPass::getPass(msg = "Gimme that token")
+
+## ---- token_secret ----
 token_secret <- getPass::getPass(msg = "Gimme that token secret")
